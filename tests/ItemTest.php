d1861e0c493e8cf363e1e7f60c794bd8fb216c46
Carsten Jonstrup <carsten@lenius.dk>
2023-08-01 01:36:15 +0200

    Bump year

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -6,14 +6,14 @@ namespace Lenius\Basket\Tests;
  * This file is part of Lenius Basket, a PHP package to handle
  * your shopping basket.
  *
- * Copyright (c) 2017 Lenius.
+ * Copyright (c) 2023 Lenius.
  * https://github.com/lenius/basket
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * @author Carsten Jonstrup<info@lenius.dk>
- * @copyright 2017 Lenius.
+ * @copyright 2023 Lenius.
  *
  * @version production
  *
